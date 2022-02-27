452ad848894888aca6a3209c8ad02fd7e3f352ed
asemelkhouli20 <asemelkhouli209@gmail.com>
2022-02-27 06:25:18 +0200

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Asem on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
